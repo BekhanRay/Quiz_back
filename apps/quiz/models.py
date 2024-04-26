4ada8d431b1f5f704517e8f08246da1a2abf9e2e
BekhanRay <bekhanrayamjanov@gmail.com>
2024-04-26 13:16:38 +0600

    innitial commit

--- apps/quiz/models.py
+++ apps/quiz/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Quiz(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date_published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_total_questions(self):
+        return self.question_set.count()
+
+    def get_total_answered_questions(self):
+        return self.question_set.filter(choice__answer__isnull=False).distinct().count()
+
+    def get_percentage_completed(self):
+        total_questions = self.get_total_questions()
+        if total_questions > 0:
+            total_answered = self.get_total_answered_questions()
+            return (total_answered / total_questions) * 100
+        return 0
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}'s answer to {self.question.question_text}: {self.choice.choice_text}"
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models
+    username = models.CharField(max_length=124, unique=True)
+    is_staff = models.BooleanField(default=False)
